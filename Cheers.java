4972773c46838aca5ca4dd26d9e0f1b0b63a4709
Yuval <yuvalshavit9@gmail.com>
2024-11-16 20:05:09 +0200

    Updated files HW2

--- Cheers.java
+++ Cheers.java
@@ -9,12 +9,23 @@ public class Cheers {
         int index = 0;
 
         while (i < length) {
-        System.out.println("Give me a " + word.charAt(index) + " : " + word.charAt(index) + "!");
+        if (word.charAt(index) == 'A' || word.charAt(index) == 'E' || word.charAt(index) == 'F' || word.charAt(index) == 'H' || word.charAt(index) ==  'I'
+        || word.charAt(index) == 'L' || word.charAt(index) == 'M' || word.charAt(index) == 'N' || word.charAt(index) ==  'O' || word.charAt(index) == 'R'
+        || word.charAt(index) == 'S' || word.charAt(index) == 'X' ) {
+                System.out.println("Give me an " + word.charAt(index) + " : " + word.charAt(index) + "!");
+        } else {
+                System.out.println("Give me a " + word.charAt(index) + " : " + word.charAt(index) + "!");
+        }
+        
         i++;
         index++;
         }
         System.out.println("What does that spell?");
 
-        System.out.println(word);
+        System.out.println(word + "!!!");
+        System.out.println(word + "!!!");
+        System.out.println(word + "!!!");
+        System.out.println(word + "!!!");
+        System.out.println(word + "!!!");
         }
 }
