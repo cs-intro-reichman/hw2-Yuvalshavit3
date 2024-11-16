a38936c519bdacf46c2089ac78017dbdd2aa2e25
Yuval <yuvalshavit9@gmail.com>
2024-11-16 19:48:15 +0200

    Updated fies HW2

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,20 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        
+        String word = args[0].toUpperCase();
+        
+        int length = word.length();
+        int i = 0;
+        int index = 0;
+
+        while (i < length) {
+        System.out.println("Give me a " + word.charAt(index) + " : " + word.charAt(index) + "!");
+        i++;
+        index++;
+        }
+        System.out.println("What does that spell?");
+
+        System.out.println(word);
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,49 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		String mode = args[1];
+		int collatz = 0;
+		int numOfRuns = 2;
+
+		if (mode.equals("v")) {
+		int number = 1;
+		for (; number <= seed; number++ ){
+			System.out.print(number + " ");
+			collatz = number;
+			if (collatz % 2 == 0) {
+				collatz = collatz / 2;
+				System.out.print(collatz + " ");
+			}
+			else {
+				collatz = collatz * 3 + 1;
+				System.out.print(collatz + " ");
+			}
+
+			for (numOfRuns = 2; collatz != 1;) {
+				
+				if (collatz % 2 == 0) {
+					collatz = collatz / 2;
+				}
+				else {
+					collatz = collatz * 3 + 1;
+				}
+				System.out.print(collatz + " ");
+				
+				numOfRuns ++;
+			
+			}
+			System.out.print("(" + numOfRuns + ")");
+			System.out.println();
+		}
+		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
 	}
-}
+
+		if (mode.equals("c")) {
+
+			System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		}
+			
+			}
+	}
+
